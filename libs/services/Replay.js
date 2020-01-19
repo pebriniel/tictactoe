012a3b4fd87c2d446dee5c6600fffb1a276af9bb
pebriniel <zeldalyoko@gmail.com>
2020-01-19 10:59:29 +0100

    [ADD] ajout des nouvelles requêtes SQL pour les replays

--- libs/services/Replay.js
+++ libs/services/Replay.js
@@ -5,11 +5,32 @@ class Replay
 
     constructor()
     {
-        this.joueur_a = null;
-        this.joueur_b = null;
+        this.player_a = null;
+        this.player_b = null;
         this.victoire_a = 0;
         this.victoire_b = 0;
     }
+
+    setPlayerA(valeur = null)
+    {
+        this.player_a = valeur;
+    }
+
+    setPlayerB(valeur = null)
+    {
+        this.player_b = valeur;
+    }
+
+    setWinA(valeur = 0)
+    {
+        this.victoire_a = valeur;
+    }
+
+    setWinB(valeur = 0)
+    {
+        this.victoire_b = valeur;
+    }
+
 
     async save(data = null)
     {
@@ -19,9 +40,9 @@ class Replay
             try{
                 let connection = await mysqlConnexion.start();
 
-                let sql_template = "INSERT INTO `replays` (joueur_a, joueur_b, victoire_a, victoire_b, data) VALUES(?, ?, ?, ?, ?) ";
+                let sql_template = "INSERT INTO `replays` (player_a, player_b, win_a, win_b, data) VALUES(?, ?, ?, ?, ?) ";
 
-                let replaces = [this.joueur_a, this.joueur_b, this.victoire_a, this.victoire_b, JSON.stringify(data)];
+                let replaces = [this.player_a, this.player_b, this.victoire_a, this.victoire_b, JSON.stringify(data)];
                 let sql = mysqlConnexion.mysql.format(sql_template, replaces);
 
                 return mysqlConnexion.insert(connection, sql);
@@ -32,26 +53,70 @@ class Replay
         }
     }
 
-    setJoueurA(valeur = null)
+    getSQLReplay(params = {})
     {
-        this.joueur_a = valeur;
+        let params_request = [];
+        let sql_template = `SELECT r.id, r.win_a, r.win_b, ua.login AS username_a, ub.login AS username_b, player_a, player_b, r.data FROM replays r
+                                                LEFT JOIN utilisateurs ub ON ub.id = r.player_b
+                                                LEFT JOIN utilisateurs ua ON ua.id = r.player_a
+                                                WHERE 1 `;
+
+        if(params['idreplay'])
+        {
+            sql_template += ' AND r.id = ?';
+            params_request.push(params['idreplay']);
+        }
+
+        if(params['player'])
+        {
+            sql_template += ' AND (r.player_a = ? || r.player_b = ?)';
+            params_request.push(params['player']);
+            params_request.push(params['player']);
+        }
+
+        return {sql: sql_template, params: params_request};
     }
 
-    setJoueurB(valeur = null)
+    async getReplay(params = null)
     {
-        this.joueur_b = valeur;
+        var mysqlConnexion = new LibMysql();
+        if(params != null)
+        {
+            try{
+                let connection = await mysqlConnexion.start();
+
+                let prepare = await this.getSQLReplay(params);
+
+                let sql = mysqlConnexion.mysql.format(prepare.sql, prepare.params);
+
+                return mysqlConnexion.queryOne(connection, sql);
+            }
+            catch{
+
+            }
+        }
     }
 
-    setVictoireA(valeur = 0)
+    async getListReplays(params = null)
     {
-        this.victoire_a = valeur;
-    }
+        var mysqlConnexion = new LibMysql();
+        if(params != null)
+        {
+            try{
 
-    setVictoireB(valeur = 0)
-    {
-        this.victoire_b = valeur;
-    }
+                let connection = await mysqlConnexion.start();
 
+                let prepare = await this.getSQLReplay(params);
+
+                let sql = mysqlConnexion.mysql.format(prepare.sql, prepare.params);
+
+                return mysqlConnexion.queryAll(connection, sql);
+            }
+            catch{
+
+            }
+        }
+    }
 }
 
 module.exports = Replay;
